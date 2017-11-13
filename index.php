c9f513ca5c4614f7bd4c826e0419af02063174d7
ZhuBrocadeSoar <zhujinteng2012@hotmail.com>
2017-11-13 20:35:43 +0800

    =>

--- index.php
+++ index.php
@@ -196,7 +196,7 @@
                 $id_seller = $row[0];
                 $sql = "INSERT INTO order_list (id_order, session_key_seller, flag_done, id_seller) VALUES ('$i', '$sessionkey', '$flag_done', '$id_seller')";
             }else{
-                $resultArray = array('fetchSuccess' => 'fail', 'failMsg' = 'No Sn Valid');
+                $resultArray = array('fetchSuccess' => 'fail', 'failMsg' => 'No Sn Valid');
             }
         }else{
             $flagQueryErr = true;
