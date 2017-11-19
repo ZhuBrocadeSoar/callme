6fabb6d92c495c71ebd291bb2f4a5a5326b43f08
ZhuBrocadeSoar <zhujinteng2012@hotmail.com>
2017-11-20 02:21:48 +0800

    mysqli error

--- index.php
+++ index.php
@@ -224,7 +224,8 @@
                     // 没有备注
                     $sql2 = "UPDATE order_list SET note_order = '$noteContent' WHERE id_order = $marchSn";
                     $retval = mysqli_query($connToMysql, $sql);
-                    $resultArray = array('pushSuccess' => 'success', 'testMsg1' => $sql2, 'testMsg2' => $retval);
+                    $mysqlierror = mysqli_error();
+                    $resultArray = array('pushSuccess' => 'success', 'testMsg1' => $sql2, 'testMsg2' => $retval, 'testMsg3' => $mysqlierror);
                 }
             }else{
                 // 无此记录
