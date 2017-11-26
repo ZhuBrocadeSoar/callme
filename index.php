ca5852039e8884b12ac8b5c763b779fc59edabff
ZhuBrocadeSoar <zhujinteng2012@hotmail.com>
2017-11-27 03:13:06 +0800

    debug

--- index.php
+++ index.php
@@ -341,8 +341,6 @@
                 $sellerName = $row[0];
                 $imageUrl = $row[1];
                 $menuList = $row[2];
-                $menuList[0] = '[';
-                $menuList[strlen($menuList) - 1] = ']';
                 $resultArray = array('infoSuccess' => 'success', 'sellerName' => $sellerName, 'imageUrl' => $imageUrl, 'menuList' => $menuList);
             }else{
                 $resultArray = array('infoSuccess' => 'fail', 'failMsg' => 'Invalid Session Error');
