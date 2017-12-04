24c74896dbbe11ae357742c4af19bc2097c98b77
ZhuBrocadeSoar <zhujinteng2012@hotmail.com>
2017-12-05 01:47:19 +0800

    debug

--- index.php
+++ index.php
@@ -546,8 +546,8 @@
                 $width = 430;
                 $auto_color = false;
                 $line_color = (object)array('r' => '0', 'g' => '0', 'b' => '0');
-                // $line_color= json_encode(array('r' => '0', 'g' => '0', 'b' => '0'));
-                $postData = array('path' => $pathWithGet, 'width' => $width, 'auto_color' => $auto_color, 'line_color' => $line_color);
+                $line_color= json_encode(array('r' => '0', 'g' => '0', 'b' => '0'));
+                // $postData = array('path' => $pathWithGet, 'width' => $width, 'auto_color' => $auto_color, 'line_color' => $line_color);
                 // var_dump($postData);
                 curl_setopt($connToWxApi, CURLOPT_URL, $url);
                 curl_setopt($connToWxApi, CURLOPT_RETURNTRANSFER, true);
