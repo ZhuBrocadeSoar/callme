f7eb23369feba71424ccf0af94026036f909d22d
ZhuBrocadeSoar <zhujinteng2012@hotmail.com>
2017-12-04 21:14:47 +0800

    debug

--- index.php
+++ index.php
@@ -535,7 +535,8 @@
                 $tokenInfoJson = substr($response, curl_getinfo($connToWxApi, CURLINFO_HEADER_SIZE));
                 // JSON 解码为数组
                 $tokenInfo = json_decode($loginInfoJson, true);
-                $token = $tokenInfo['access_token'];
+                // $token = $tokenInfo['access_token'];
+                $token = $tokenInfo;
                 $url = "https://api.weixin.qq.com/wxa/getwxacode?access_token=$token";
                 // 获取二维码
                 /*$connToWxApi = curl_init();
