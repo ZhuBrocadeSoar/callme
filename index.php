fb1e1d0341eb2c84c313bf19b503dfb9ea24f518
ZhuBrocadeSoar <zhujinteng2012@hotmail.com>
2017-12-04 22:56:23 +0800

    debug

--- index.php
+++ index.php
@@ -545,7 +545,7 @@
                 $pathWithGet = 'pages/qu/qu?sellerId=' . strval($sellerId);
                 $width = 430;
                 $auto_color = false;
-                $line_color = json_encode(array('r' => '0', 'g' => '0', 'b' => '0'));
+                $line_color = (object)json_encode(array('r' => '0', 'g' => '0', 'b' => '0'));
                 $postData = array('path' => $pathWithGet, 'width' => $width, 'auto_color' => $auto_color, 'line_color' => $line_color);
                 var_dump($postData);
                 curl_setopt($connToWxApi, CURLOPT_URL, $url);
